96fbadd28e53199382fff479ee5939692165d74f
soh <cherweisoh@Cherwei-Sohs-MacBook.local>
2010-05-26 13:28:34 +0800

    basic

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,41 @@
+class BookingsController < ApplicationController
+  def index
+    render
+  end
+  
+  def create
+    if @booking = Booking.find_by_phone(params[:booking][:phone])
+      if @booking.claimed?
+        flash[:notice] = 'This booking had been claimed.'
+        redirect_to bookings_url
+      else
+        redirect_to booking_url(@booking)
+      end
+    else
+      flash[:notice] = 'Invalid phone number.'
+      redirect_to bookings_url
+    end
+  end
+  
+  def show
+    @booking = Booking.find(params[:id])
+    if @booking.claimed?
+      flash[:notice] = 'This booking had been claimed.'
+      redirect_to bookings_url
+    else
+      render
+    end
+  end
+  
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.claimed?
+      flash[:notice] = 'This booking had been claimed.'
+      redirect_to bookings_url
+    else
+      @booking.update_attribute!(:claimed, true)
+      flash[:notice] = "#{@booking.phone} claimed!"
+      redirect_to bookings_url
+    end
+  end
+end
---
+++ db/migrate/20100526040341_create_bookings.rb
@@ -0,0 +1,16 @@
+class CreateBookings < ActiveRecord::Migration
+  def self.up
+    create_table :bookings do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.boolean :claimed, :default => false
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :bookings
+  end
+end
